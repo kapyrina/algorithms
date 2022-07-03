41cfb0beeda8b25f886e510b4c840dd745b55562
Alina <98577574+kapyrina@users.noreply.github.com>
2022-07-03 22:35:43 +0300

    Add files via upload

---
+++ 2 semester/task_8.cpp
@@ -0,0 +1,70 @@
+﻿#include <clocale>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+int recursFindWord(string Array[], string keyWord, int Ind)
+{
+	if (Array[Ind] != keyWord)
+	{
+		Ind++;
+		return recursFindWord(Array, keyWord, Ind);
+	}
+	else
+	{
+		return Ind + 1;
+	}
+}
+
+int main()
+{
+	int Ind = 0;
+	string  keyWord;
+	string Array[20] =
+	{
+		"Scott",
+		"Klaus",
+		"Marty",
+		"Jules",
+		"Michael",
+		"Vi",
+		"Tyler",
+		"Turkish",
+		"Alexey",
+		"Shurik",
+		"James",
+		"Aldo",
+		"Fox",
+		"JD",
+		"Igor",
+		"Jack",
+		"Gustave",
+		"Jay",
+		"Elton",
+		"Holland"
+	};
+
+	for (int i = 0; i < 20; i++)
+	{
+		cout << Array[i] << endl;
+	}
+
+	cout << "Enter a name to search in massive: ";
+	cin >> keyWord;
+
+	Ind = recursFindWord(Array, keyWord, Ind);
+
+	if (Ind != 21)
+	{
+		cout << "Name " << keyWord << " in massive located on the " << Ind << " place.";
+	}
+	else
+	{
+		cout << "Name " << keyWord << " in massive not found";
+	}
+
+	cin.get();
+	cin.get();
+}
