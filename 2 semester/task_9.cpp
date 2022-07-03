e3c31190d34a0e78706f8f7a8e2c1a551a586f23
Alina <98577574+kapyrina@users.noreply.github.com>
2022-07-03 22:45:34 +0300

    Add files via upload

---
+++ 2 semester/task_9.cpp
@@ -0,0 +1,52 @@
+﻿#include <clocale>
+#include <iostream>
+#include "windows.h"
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int n = 100;
+    int Array[100];
+    int k = 1;
+    cout << "One-dimensional array of 100 elements: ";
+    cout << endl;
+
+    for (int i = 0; i < 100; i++)
+    {
+        Array[i] = rand() % 100;
+        cout << Array[i] << endl;
+    }
+
+    bool swapped;
+    for (int i = 0; i < n - 1; i++)
+    {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (Array[j] > Array[j + 1])
+            {
+                int temp = Array[j];
+                Array[j] = Array[j + 1];
+                Array[j + 1] = temp;
+                swapped = true;
+            }
+        }
+
+        if (swapped == false)
+            break;
+    }
+
+    cout << "One-dimensional sorted (Bubble sort) array of 100 elements: ";
+    cout << endl;
+
+    for (int i = 0; i < 100; i++)
+    {
+        cout << Array[i] << endl;
+    }
+
+    cin.get();
+    cin.get();
+}
---
+++ 2 semester/task_9_2.cpp
@@ -0,0 +1,92 @@
+﻿#include <clocale>
+#include <iostream>
+#include "windows.h"
+
+using namespace std;
+
+void merge(int merged[], int lenD, int L[], int lenL, int R[], int lenR)
+{
+    int i = 0;
+    int j = 0;
+    while (i < lenL || j < lenR)
+    {
+        if (i < lenL && j < lenR)
+        {
+            if (L[i] <= R[j])
+            {
+                merged[i + j] = L[i];
+                i++;
+            }
+            else
+            {
+                merged[i + j] = R[j];
+                j++;
+            }
+        }
+        else if (i < lenL)
+        {
+            merged[i + j] = L[i];
+            i++;
+        }
+        else if (j < lenR)
+        {
+            merged[i + j] = R[j];
+            j++;
+        }
+    }
+}
+
+void mergeSort(int data[], int lenD)
+{
+    if (lenD > 1)
+    {
+        int middle = lenD / 2;
+        int rem = lenD - middle;
+        int* L = new int[middle];
+        int* R = new int[rem];
+        for (int i = 0; i < lenD; i++)
+        {
+            if (i < middle)
+            {
+                L[i] = data[i];
+            }
+            else
+            {
+                R[i - middle] = data[i];
+            }
+        }
+        mergeSort(L, middle);
+        mergeSort(R, rem);
+        merge(data, lenD, L, middle, R, rem);
+    }
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int n = 100;
+    int Array[100];
+    int k = 1;
+    cout << "One-dimensional array of 100 elements: ";
+    cout << endl;
+
+    for (int i = 0; i < 100; i++)
+    {
+        Array[i] = rand() % 100;
+        cout << Array[i] << endl;
+    }
+
+    mergeSort(Array, 100);
+
+    cout << "One-dimensional sorted (Bose-Nelsone sort) array of 100 elements: ";
+    cout << endl;
+
+    for (int i = 0; i < 100; i++)
+    {
+        cout << Array[i] << endl;
+    }
+
+    cin.get();
+    cin.get();
+}
